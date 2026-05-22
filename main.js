a5dad683d020986fd53a43d8044fbd0cea45f311
Miguel Ardila <ardila16394@gmail.com>
2026-05-22 13:40:29 -0500

    corregido error que no permitía ver los planeamientos sin haber iniciado sesión en la página

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { db } from "./firebase-config.js";
+import { db } from "./firebase-public-config.js";
 import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 let darkMode = false;
