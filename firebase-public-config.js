59d88c9e8f6c7c33c26594a520172a620a8a84c3
Miguel Ardila <ardila16394@gmail.com>
2026-05-22 12:44:06 -0500

    arreglos menores

---
+++ firebase-public-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCUFKtck5zFw4jy0hmogflgtZaj68_TaOM",
+    authDomain: "urologiaudes.firebaseapp.com",
+    projectId: "urologiaudes",
+    storageBucket: "urologiaudes.firebasestorage.app",
+    messagingSenderId: "593233184412",
+    appId: "1:593233184412:web:cbefe676a60d68b23fcf59",
+    measurementId: "G-42LW1EJNG4"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
